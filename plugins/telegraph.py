fc4f8478946c42009552bb924859d98bfe44689c
Not faund <flaelva6@gmail.com>
2025-07-22 22:44:47 +0700

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from KNMusic import app
+from DanteMusic import app
 from TheApi import api
 
 
